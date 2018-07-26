beef688851c38ae38c9ee4f232be03fa25aab1a4
Fernando Mota e Silva <fm.aken@gmail.com>
2018-07-26 12:26:43 -0300

    Initial Commit

---
+++ Schedulers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schedulers
+//
+//  Created by Fernando Mota e Silva on 26/07/2018.
+//  Copyright © 2018 HalphDem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
