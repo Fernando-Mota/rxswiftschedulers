208e357c7a9c900f359d127acc0ca57596c291d3
Fernando Mota e Silva <fm.aken@gmail.com>
2018-09-01 13:52:37 -0300

    Talk sobre schedulers em rxswift

---
+++ Podfile
@@ -0,0 +1,6 @@
+# Podfile
+use_frameworks!
+
+target 'Schedulers' do
+    pod 'RxSwift',    '~> 4.0'
+end
--- Schedulers/ViewController.swift
+++ Schedulers/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    let background = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+    
+    let observable = Observable<Int>.create { observer in
+        //CODIGO DE SUBSCRIPTION
+        if Thread.current.isMainThread {
+            print("[ELEMENT] Executando na main thread!\n\n")
+        } else {
+            print("[ELEMENT] Executando em ota thread!\n\n")
+        }
+        observer.onNext(1)
+        sleep(2)
+        observer.onCompleted()
+        return Disposables.create()
+    }
+    
+    let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        observable
+            .map { [weak self] value -> String in
+                self?.whichThread(local: "MAP")
+                return "\(value)"
+            }
+            .subscribeOn(background)
+            .observeOn(MainScheduler.instance)
+            .filter { [weak self] value in
+                self?.whichThread(local: "FILTER")
+                return "2" == value || "1" == value
+            }
+            
+            .subscribe(onNext: { [weak self] _ in
+                //CODIGO DE OBSERVATION
+                self?.whichThread(local: "SUBSCRIBE")
+            }, onError: { erro in
+                print("Deu ruim")
+            }, onCompleted: { [weak self] in
+                //CODIGO DE OBSERVATION
+                self?.whichThread(local: "COMPLETED")
+            }).disposed(by: bag)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func whichThread(local: String) {
+        if Thread.current.isMainThread {
+            print("[\(local)] Executando na main thread!\n\n")
+        } else {
+            print("[\(local)] Executando em ota thread!\n\n")
+        }
     }
-
 
 }
 
